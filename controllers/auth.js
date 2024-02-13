3119f2557d05a08b0ca74f8df2f14dff7927b0f7
james <jameslindsay2220@gmail.com>
2024-02-13 01:03:41 +0000

    changed file paths to fix errors, still having error with jwt from jsonWebToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
-import { Jwt } from "jsonwebtoken";
-import User from "./models/User.js";
+import { jwt } from "jsonwebtoken";
+import User from "../models/User.js";
 
 // REGISTER USER
 
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,4 @@
-import Post from "../models/Post.Js";
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 //CREATE
