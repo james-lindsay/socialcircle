83c58e20863405b9af931cbee8c03adcd6df6fd0
james <jameslindsay2220@gmail.com>
2024-02-13 01:12:00 +0000

    fixed bugs on server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import register from "./controllers/auth.js";
+import { register } from "./controllers/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { createPost } from "./controllers/posts.js";
